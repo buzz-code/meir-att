998104c2fad0224c8a3c3fc2b145e74e638d08fa
buzz-code <72219643+buzz-code@users.noreply.github.com>
2023-06-08 21:17:51 +0300

    update common-modules & use new operator

--- client/containers/student-percent-report/StudentPercentReportContainer.js
+++ client/containers/student-percent-report/StudentPercentReportContainer.js
@@ -97,7 +97,7 @@ const getFilters = ({ students, teachers, klasses, lessons }) => [
     type: 'date',
     operator: 'date-after',
   },
-  { field: 'half', label: 'מחצית', type: 'text', operator: 'like' },
+  { field: 'half', label: 'מחצית', type: 'text', operator: 'like-or-null' },
   // { field: 'abs_count', label: 'חיסורים', type: 'text', operator: 'like' },
   // { field: 'approved_abs_count', label: 'חיסורים מאושרים', type: 'text', operator: 'like' },
   // { field: 'sheet_name', label: 'חודש דיווח', type: 'text', operator: 'like' },
